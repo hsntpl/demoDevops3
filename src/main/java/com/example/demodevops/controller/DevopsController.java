f4d5f11ad0a3267236a0a134d60ff2c929e2da94
hsntpl <hasantoplu02@gmail.com>
2026-01-31 13:05:21 +0300

    versiyon 1.0.3
    versiyon 3 oldu

--- src/main/java/com/example/demodevops/controller/DevopsController.java
+++ src/main/java/com/example/demodevops/controller/DevopsController.java
@@ -10,11 +10,11 @@ public class DevopsController {
 
     @GetMapping
     public String devopsHello(){
-        return "Hello Devops HSN 1.0.2";
+        return "Hello Devops HSN 1.0.3";
     }
 
     @GetMapping("info")
     public String info(){
-        return "Hello INFO HSN versiyon 1.0.2 oldu"  + LocalDateTime.now();
+        return "Hello INFO HSN versiyon 1.0.3 oldu"  + LocalDateTime.now();
     }
 }
